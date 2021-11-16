33f63ddead557aa14506a415106c44c7d2be601e
Pedram Rahmanian <pedram@gutenmade.com>
2021-11-16 15:59:39 -0800

    refactored App to use Yelp

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -5,34 +5,44 @@ import SearchBar from '../SearchBar/SearchBar'
 import Yelp from '../../util/Yelp'
 
 // This is static data for use in building the Business component.
-const business = {
-  imageSrc: 'https://content.codecademy.com/programs/react/ravenous/pizza.jpg',
-  name: 'MarginOtto Pizzeria',
-  address: '1010 Paddington Way',
-  city: 'Flavortown',
-  state: 'NY',
-  zipCode: '10101',
-  category: 'Italian',
-  rating: 4.5,
-  reviewCount: 90
-}
+// const business = {
+//   imageSrc: 'https://content.codecademy.com/programs/react/ravenous/pizza.jpg',
+//   name: 'MarginOtto Pizzeria',
+//   address: '1010 Paddington Way',
+//   city: 'Flavortown',
+//   state: 'NY',
+//   zipCode: '10101',
+//   category: 'Italian',
+//   rating: 4.5,
+//   reviewCount: 90
+// }
 
 // This is a static array for use in passing data between components.
-const businesses = [
-  business,
-  business,
-  business,
-  business,
-  business,
-  business
-]
+// const businesses = [
+//   business,
+//   business,
+//   business,
+//   business,
+//   business,
+//   business
+// ]
 
 
 class App extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      businesses: []
+    }
+    this.searchYelp = this.searchYelp.bind(this)
+  }
 
   searchYelp(term, location, sortBy) {
     // simulated search function
-    console.log(` Searching Yelp with... term: ${term},   location: ${location},  sort:${sortBy}`)
+    // console.log(` Searching Yelp with... term: ${term},   location: ${location},  sort:${sortBy}`)
+    Yelp.search(term, location, sortBy).then(businesses => {
+      this.setState({businesses: businesses})
+    })
   }
 
   render() {
@@ -40,7 +50,7 @@ class App extends React.Component {
       <div className='APP'>
           <h1>ravenous</h1>
           <SearchBar searchYelp={this.searchYelp} />
-          <BusinessList businesses={businesses}/>
+          <BusinessList businesses={this.state.businesses}/>
       </div>
     )
   }
