a932cff7999412da4be080c3c3fa4f1dd9b826de
Pedram Rahmanian <pedram@gutenmade.com>
2021-11-16 13:47:20 -0800

    Added constructor and state to SearchBar

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -1,20 +1,27 @@
 import React from 'react'
 import './SearchBar.css'
 
-// Options to sort Yelp API results
-// https://www.yelp.com/developers/documentation/v3/business_search
-
-const sortByOptions = {
-    "Best Match": "best_match",
-    "Highest Rated": "rating",
-    "Most Reviewed": "review_count",
-}
-
 class SearchBar extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            term: '',
+            location: '',
+            sortBy: 'best_match',
+        }
+
+        // Options to sort Yelp API results
+        // https://www.yelp.com/developers/documentation/v3/business_search
+        this.sortByOptions = {
+            "Best Match": "best_match",
+            "Highest Rated": "rating",
+            "Most Reviewed": "review_count",
+        }
+    }
 
     renderSortByOptions() {
-        return Object.keys(sortByOptions).map(sortByOption => {
-            let sortByOptionValue = sortByOptions[sortByOption]
+        return Object.keys(this.sortByOptions).map(sortByOption => {
+            let sortByOptionValue = this.sortByOptions[sortByOption]
             return <li key={sortByOptionValue}>{sortByOption}</li>
         })
     }
