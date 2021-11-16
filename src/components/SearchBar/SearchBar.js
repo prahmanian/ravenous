2b6c09ad2a66398a7d5086b276200d9e6248f825
Pedram Rahmanian <pedram@gutenmade.com>
2021-11-16 14:20:24 -0800

    added handleSearch method to SearchBar

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -11,6 +11,7 @@ class SearchBar extends React.Component {
         }
         this.handleTermChange = this.handleTermChange.bind(this)
         this.handleLocationChange = this.handleLocationChange.bind(this)
+        this.handleSearch = this.handleSearch.bind(this)
 
         // Options to sort Yelp API results
         // https://www.yelp.com/developers/documentation/v3/business_search
@@ -38,6 +39,11 @@ class SearchBar extends React.Component {
 
     handleLocationChange(event) {
         this.setState({location: event.target.value})
+    }
+
+    handleSearch(event) {
+        this.props.searchYelp(this.state.term, this.state.location, this.state.sortBy)
+        event.preventDefault()
     }
 
     renderSortByOptions() {
@@ -69,7 +75,7 @@ class SearchBar extends React.Component {
                     <input placeholder="Where?" onChange={this.handleLocationChange} />
                 </div>
                 <div className="SearchBar-submit">
-                    <a>Let's Go</a>
+                    <a onClick={this.handleSearch}>Let's Go</a>
                 </div>
 
             </div>
