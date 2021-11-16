605832545f932e6fb24f167de5eca65f335d8bf5
Pedram Rahmanian <pedram@gutenmade.com>
2021-11-16 13:29:35 -0800

    refactored static data into App

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,7 +3,28 @@ import './App.css';
 import BusinessList from '../BusinessList/BusinessList'
 import SearchBar from '../SearchBar/SearchBar'
 
-import logo from '../../logo.svg';
+// This is static data for use in building the Business component.
+const business = {
+  imageSrc: 'https://content.codecademy.com/programs/react/ravenous/pizza.jpg',
+  name: 'MarginOtto Pizzeria',
+  address: '1010 Paddington Way',
+  city: 'Flavortown',
+  state: 'NY',
+  zipCode: '10101',
+  category: 'Italian',
+  rating: 4.5,
+  reviewCount: 90
+}
+
+// This is a static array for use in passing data between components.
+const businesses = [
+  business,
+  business,
+  business,
+  business,
+  business,
+  business
+]
 
 
 class App extends React.Component {
