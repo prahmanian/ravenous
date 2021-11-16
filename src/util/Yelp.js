dfecf6a88b9aa9d9d1bdde44d267d9a2df57b6d7
Pedram Rahmanian <pedram@gutenmade.com>
2021-11-16 15:49:04 -0800

    created Yelp object to handle API search

--- src/util/Yelp.js
+++ src/util/Yelp.js
@@ -1,1 +1,37 @@
 import {yelpAPIKey} from './_Credentials'
+
+const Yelp = {
+    search(term, location, sortBy) {
+        const endpoint = `https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`
+        const CORSAnywhere = 'https://cors-anywhere.herokuapp.com/'
+
+        const settings = {
+            headers: {
+                Authorization: `Bearer ${yelpAPIKey}`
+            }
+        }
+        return fetch(CORSAnywhere+endpoint, settings).then(
+            response => {return response.json()}
+        ).then( jsonResponse => {
+            if(jsonResponse.businesses) {
+                return jsonResponse.businesses.map(business => {
+                    console.log(business)
+                    return {
+                        id: business.id,
+                        imageSrc: null,
+                        name: null,
+                        address: null,
+                        city: null,
+                        state: null,
+                        zipCode: null,
+                        category: null,
+                        rating: null,
+                        reviewCount: null,
+                    }
+                })
+            }
+        })
+    }
+}
+
+export default Yelp
