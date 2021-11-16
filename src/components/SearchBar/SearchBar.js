150e6f0477762f737073db6d48e4b465cefab8fd
Pedram Rahmanian <pedram@gutenmade.com>
2021-11-16 14:08:01 -0800

    added onChange handlers for input fields

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -9,6 +9,8 @@ class SearchBar extends React.Component {
             location: '',
             sortBy: 'best_match',
         }
+        this.handleTermChange = this.handleTermChange.bind(this)
+        this.handleLocationChange = this.handleLocationChange.bind(this)
 
         // Options to sort Yelp API results
         // https://www.yelp.com/developers/documentation/v3/business_search
@@ -28,6 +30,14 @@ class SearchBar extends React.Component {
 
     handleSortByChange(sortByOption) {
         this.setState({sortBy: sortByOption})
+    }
+
+    handleTermChange(event) {
+        this.setState({term: event.target.value})
+    }
+
+    handleLocationChange(event) {
+        this.setState({location: event.target.value})
     }
 
     renderSortByOptions() {
@@ -55,8 +65,8 @@ class SearchBar extends React.Component {
                     </ul>
                 </div>
                 <div className="SearchBar-fields">
-                    <input placeholder="Search Businesses" />
-                    <input placeholder="Where?" />
+                    <input placeholder="Search Businesses" onChange={this.handleTermChange} />
+                    <input placeholder="Where?" onChange={this.handleLocationChange} />
                 </div>
                 <div className="SearchBar-submit">
                     <a>Let's Go</a>
